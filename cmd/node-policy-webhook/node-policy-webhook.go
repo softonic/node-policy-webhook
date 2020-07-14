d26a58d39f55fc0ef4771db3ce967db721c1dd9d
Santiago Núñez-Cacho <santiago.nunezcacho@softonic.com>
2020-07-14 11:39:45 +0200

    arguments for cert and key

--- cmd/node-policy-webhook/node-policy-webhook.go
+++ cmd/node-policy-webhook/node-policy-webhook.go
@@ -20,6 +20,9 @@ type params struct {
 
 func main() {
 	var params params
+	var certificate string
+	var privateKey string
+
 	commandName := path.Base(os.Args[0])
 
 	rootCmd := &cobra.Command{
@@ -29,20 +32,28 @@ func main() {
 			if params.version {
 				fmt.Println("Version:", version.Version)
 			} else {
-				run(&params)
+				run(&params, certificate, privateKey)
 			}
 		},
 	}
 	rootCmd.Flags().BoolVarP(&params.version, "version", "v", false, "print version and exit")
+	rootCmd.Flags().StringVarP(&certificate, "tls-cert", "c", "default", "certificate (required)")
+	rootCmd.Flags().StringVarP(&privateKey, "tls-key", "p", "default", "privateKey (required)")
+
+	rootCmd.MarkFlagRequired("tls-cert")
+	rootCmd.MarkFlagRequired("tls-key")
+
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func run(params *params) {
+func run(params *params, certificate string, privateKey string) {
 
-	_, err := tls.LoadX509KeyPair("/etc/webhook/certs/cert.pem", "/etc/webhook/certs/key.pem")
+	//_, err := tls.LoadX509KeyPair("/etc/webhook/certs/cert.pem", "/etc/webhook/certs/key.pem")
+	_, err := tls.LoadX509KeyPair(certificate, privateKey)
 	if err != nil {
 		klog.Errorf("Failed to load key pair: %v", err)
 	}
@@ -50,7 +61,7 @@ func run(params *params) {
 	http.HandleFunc("/mutate", func(w http.ResponseWriter, r *http.Request) {
 		node_policy.HttpHandler(w, r)
 	})
-	if err := http.ListenAndServeTLS(":443", "/etc/webhook/certs/cert.pem", "/etc/webhook/certs/key.pem", nil); err != nil {
+	if err := http.ListenAndServeTLS(":443", certificate, privateKey, nil); err != nil {
 		log.Println(err)
 		klog.Errorf("Failed to listen and serve webhook server: %v", err)
 
